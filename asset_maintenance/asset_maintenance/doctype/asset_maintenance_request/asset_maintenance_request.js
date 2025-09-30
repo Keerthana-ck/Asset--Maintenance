6e0c97fb489295d409952f3290c9cb9a092ad78f
keerthana <keerthanachandrank@Keerthanas-MacBook-Air.local>
2025-09-30 12:54:36 +0530

    fix : Add Filter in asset field

--- asset_maintenance/asset_maintenance/doctype/asset_maintenance_request/asset_maintenance_request.js
+++ asset_maintenance/asset_maintenance/doctype/asset_maintenance_request/asset_maintenance_request.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2025, keerthana and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Asset Maintenance Request", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Asset Maintenance Request", {
+    refresh: function(frm) {
+        frm.set_query("asset", function() {
+            return {
+                filters: {
+                    custom_in_use: 1
+                }
+            };
+        });
+    }
+});
