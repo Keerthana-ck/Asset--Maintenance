860b8539f70f592b20c0b55104f482fe2401125c
keerthana <keerthanachandrank@Keerthanas-MacBook-Air.local>
2025-09-30 23:21:47 +0530

    fix: Create email notification to Maintenance Supervisor to immidiate notify when priority is Urgent

--- asset_maintenance/asset_maintenance/doctype/asset_maintenance_request/asset_maintenance_request.py
+++ asset_maintenance/asset_maintenance/doctype/asset_maintenance_request/asset_maintenance_request.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2025, keerthana and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils.user import get_users_with_role
 
 
 class AssetMaintenanceRequest(Document):
-	pass
+	def after_insert(self):
+		self.send_email()
+
+	def send_email(self):
+		"""Method to send email notification to Maintenance Supervisor , if the asset maintenance request is Urgent """
+		if self.priority == "Urgent":
+			recipients = get_users_with_role('Maintenance Supervisor')
+			if recipients:
+				subject = f"Urgent!! Asset Maintenance Request {self.name}"
+				message = f"Asset Maintenance Request {self.name}. Please take necessary action."
+				frappe.sendmail(
+	                recipients = recipients,
+	                subject = subject,
+	                message = message,
+	                delayed = False
+				)
